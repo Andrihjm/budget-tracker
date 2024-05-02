ac403f36d0c742e1e5f5f12cf3f970a0379b712d
andri <andri@pop-os.localdomain>
2024-05-02 15:04:06 +0700

    create transaction component

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,11 +1,54 @@
-'use client'
+"use client";
 
-import React from 'react'
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constant";
+import { differenceInDays, startOfMonth } from "date-fns";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import TransactionTable from "./_components/TransactionTable";
 
-const page = () => {
+const TransactionsPage = () => {
+  const [isDateRange, setIsDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
   return (
-    <div>page</div>
-  )
-}
+    <>
+      {/* ---------- HEADER ---------- */}
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <div>
+            <p className="text-3xl font-bold">Transactions History</p>
+          </div>
+        </div>
 
-export default page
+        <DateRangePicker
+          initialDateFrom={isDateRange.from}
+          initialDateTo={isDateRange.to}
+          showCompare={false}
+          onUpdate={(values) => {
+            const { from, to } = values.range;
+            // Perbaharui rentang tanggal hanya jika kedua tangga di gunakaan
+
+            if (!from || !to) return;
+            if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+              toast.error(
+                `The selected date range is to big. Max allowe range is ${MAX_DATE_RANGE_DAYS} days!`
+              );
+              return;
+            }
+
+            setIsDateRange({ from, to });
+          }}
+        />
+      </div>
+      {/* ---------- END HEDER ---------- */}
+
+      <div className="container">
+        <TransactionTable from={isDateRange.from} to={isDateRange.to} />
+      </div>
+    </>
+  );
+};
+
+export default TransactionsPage;
